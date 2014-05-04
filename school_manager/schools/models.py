0bc55bef864d975368cd79cf75ed35f93fb9ef78
Alex Kyllo <alex.kyllo@gmail.com>
2014-05-04 12:28:30 -0700

    added some basic models and a test case

--- school_manager/schools/models.py
+++ school_manager/schools/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Person(models.Model):
+    user = models.OneToOneField(User)
+
+class School(models.Model):
+    name = models.CharField(max_length=80)
+
+class Location(models.Model):
+    school = models.ForeignKey(School)
+    name = models.CharField(max_length=50)
+    managers = models.ManyToManyField(Person)
+    address_1 = models.CharField(max_length=50)
+    address_2 = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state_province = models.CharField(max_length=4)
+    zip_postal_code = models.CharField(max_length=10)
+    country = models.CharField(max_length=2)
+
+class Course(models.Model):
+    location = models.ForeignKey(Location)
+    instructors = models.ManyToManyField(Person, related_name="instructors")
+    students = models.ManyToManyField(Person, related_name="students")
+
+class Session(models.Model):
+    course = models.ForeignKey(Course)
+    students = models.ManyToManyField(Person)
+	
+	
--- school_manager/schools/tests.py
+++ school_manager/schools/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from schools.models import *
 
 # Create your tests here.
+
+class NewSchoolLocation(TestCase):
+	def setUp(self):
+		cool_school = School.objects.create(name="A Cool School")
+		Location.objects.create(school=cool_school, name="Kirkland")
+	
+	def testSchoolHasLocation(self):
+		cool_school = School.objects.get(name="A Cool School")
+		location = Location.objects.get(name="Kirkland")
+		self.assertEqual(location.school, cool_school)
