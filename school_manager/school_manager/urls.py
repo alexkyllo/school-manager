06ed7a52fb6cd564dfb8d008f309a44716c321aa
Alex Kyllo <alex.kyllo@gmail.com>
2014-05-08 23:32:45 -0700

    added first templates

--- school_manager/school_manager/urls.py
+++ school_manager/school_manager/urls.py
@@ -6,9 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'school_manager.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'schools.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^schools/$', SchoolList.as_view())
 )
--- school_manager/schools/views.py
+++ school_manager/schools/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
-
+from django.template import RequestContext, loader
+from django.http import HttpResponse
 # Create your views here.
 from django.views.generic import ListView
 from schools.models import School
 
 class SchoolList(ListView):
     model = School
+
+
+def home(request):
+	template = loader.get_template('schools/index.html')
+	context = RequestContext(request, {
+	})
+	return HttpResponse(template.render(context))
+
 
