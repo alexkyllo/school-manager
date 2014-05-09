70be1a3dc0b4e5b2e60274962fa506f65d366c9b
Alex Kyllo <alex.kyllo@gmail.com>
2014-05-09 07:05:39 -0700

    added simple CBV DetailView for Schools

--- school_manager/school_manager/urls.py
+++ school_manager/school_manager/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from schools.views import SchoolList
+from schools.views import SchoolList, SchoolDetail
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'schools.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^schools/$', SchoolList.as_view())
+    url(r'^schools/$', SchoolList.as_view()),
+    url(r'^schools/(?P<pk>\d+)/$', SchoolDetail.as_view())
 )
--- school_manager/schools/views.py
+++ school_manager/schools/views.py
@@ -1,11 +1,14 @@
 from django.template import RequestContext, loader
 from django.http import HttpResponse
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from schools.models import School
 
 class SchoolList(ListView):
     model = School
+
+class SchoolDetail(DetailView):
+	model = School
 
 
 def home(request):
