61a74685f7fc413ceb0f32d3688b7b8dfeee86be
Jesse Ruth <madongo@gmail.com>
2014-05-10 08:37:06 -0700

    modified:   school_manager/settings.py - Set the project up for use with postgres
    modified:   schools/admin.py - Registered the current models to the admin

--- school_manager/schools/admin.py
+++ school_manager/schools/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person, School, Location, Course, Session
+
+admin.site.register(Person)
+admin.site.register(School)
+admin.site.register(Location)
+admin.site.register(Course)
+admin.site.register(Session)
