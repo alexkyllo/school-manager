521ddbcf4b4e4f36b1af55ef8923cf644bd87e78
Jesse Ruth <madongo@gmail.com>
2014-05-13 20:32:56 -0700

    creating some analytics for the system

---
+++ school_manager/analytics/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Analysis(models.Model):
+    """rying to start an analytics app"""
+    name = models.CharField(max_length=20)
+    analysis_title = models.CharField(max_length=30)
+    user = models.CharField(max_length=30)
+    startdatetime = models.DateTimeField()
+    enddatetime = models.DateTimeField()
+    group = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        """Name of the model class"""
+        return self.name
+
+
+    class Meta:
+        verbose_name_plural = "Analyses"
