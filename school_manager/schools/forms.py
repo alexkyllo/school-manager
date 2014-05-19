f470e3fbf7a616bf30dd0f4808e135aa127a5b47
Alex Kyllo <alex.kyllo@gmail.com>
2014-05-19 06:41:44 -0700

    added instructor and student creation forms

--- school_manager/schools/forms.py
+++ school_manager/schools/forms.py
@@ -27,3 +27,23 @@ class ManagerCreationForm(UserCreationForm):
             user.save()
             user.groups.add(managers)
         return user
+
+class StudentCreationForm(UserCreationForm):
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        students = Group.objects.get(name='Students')
+        if commit:
+            user.save()
+            user.groups.add(students)
+        return user
+
+class InstructorCreationForm(UserCreationForm):
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        instructors = Group.objects.get(name='Instructors')
+        if commit:
+            user.save()
+            user.groups.add(instructors)
+        return user
