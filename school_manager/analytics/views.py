a1afb0d71040fdc9ed062c3d6bc0a024a2ee95de
Jesse Ruth <madongo@gmail.com>
2014-05-21 19:43:29 -0700

    Added Pandas and working on getting the model names

--- school_manager/analytics/views.py
+++ school_manager/analytics/views.py
@@ -22,24 +22,33 @@ def simple_chart(request):
     """The first try at creating analytics"""
     import random
     import django
+    from schools.models import School
     import numpy as np
+    import pandas as pd
     from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
     from matplotlib.figure import Figure
+    from django.forms.models import model_to_dict
 
     # Specific Variables
+    filtered_data = School.objects.all().values().exclude(name='Jims') #.filter(name='Jims')
+    model_names = School._meta.get_all_field_names()
+    ddict = model_to_dict(School.objects.get(pk=1))
+    ndict = []
+    ndict = ddict.keys
+    df = pd.DataFrame.from_records(filtered_data)
+    df['y'] = random.randint(0, 1000)
+    #df    = df[ df.name != 'Jims']
     h = 5
     w = 8
     x = []
     y = []
-    labels = []
     chart_label = "First Analytic Chart Label"
     xlabel = "This is the X Label"
     ylabel = "This is the y label"
 
     # Get the data from the database
-    schools = School.objects.all()
-    num_obj_returned = len(schools)
-    ind = np.arange(num_obj_returned)
+    num_obj_returned = len(model_names)
+    chart_index = np.arange(num_obj_returned)
 
     # Create a figure object
     fig = Figure()
@@ -48,16 +57,18 @@ def simple_chart(request):
     fig.set_frameon(False)
     # create an axis
     ax = fig.add_subplot(111)
-
+    new_labels = df.name
     for i in range(num_obj_returned):
-        labels.append(str(schools[i].name))
         y.append(random.randint(0, 1000))
     # Create a bar chart
-    ax.bar(ind - .25, y, color='grey', width=.5)
+    x_var = chart_index - .25
+    y_var = y
+    ax.bar(x_var, y_var, color='grey', width=.5)
     ax.set_xlim(xmin=-0.25, xmax=num_obj_returned - 0.75)
+
     # Where the x ticks are placed and set the labels
-    ax.set_xticks(ind)
-    ax.set_xticklabels(labels, ha='center')
+    ax.set_xticks(chart_index)
+    ax.set_xticklabels(model_names, ha='center')
     # Set the chart title
     ax.set_title(chart_label, loc="center")
     # Set the x and y labels
